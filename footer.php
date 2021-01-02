f7921942595a1e96303a725433b26ccc019b4f9d
Gentille Plume <melvin.lemoine@feather-project.org>
2021-01-02 19:41:06 +0100

    Minor improvements

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <?php wp_footer(); ?>
 
-<footer class="footer"></footer>
+<footer></footer>
 
 
 </body>
--- home.php
+++ home.php
@@ -1,13 +1,11 @@
 <?php get_header(); ?>
 
 <?php if(have_posts()) : ?>
-
     <?php while (have_posts()) : the_post(); ?>
 
       <article></article>
 
     <?php endwhile; ?>
-
 <?php endif; ?>
 
 <?php get_footer(); ?>
