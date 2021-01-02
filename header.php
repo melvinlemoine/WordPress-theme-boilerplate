32b475bd0d7ba9d7e850e2122c7dfb110bd5a301
Gentille Plume <melvin.lemoine@feather-project.org>
2021-01-02 19:22:44 +0100

    Created header.php

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title><?php the_title(); ?></title>
+
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height" />
+
+    <?php wp_head(); ?>
+
+</head>
+
+
+<body <?php body_class(); ?>>
