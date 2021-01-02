173c19581051cbd799b3533a130f84fca45d64bc
Gentille Plume <melvin.lemoine@feather-project.org>
2021-01-02 19:37:26 +0100

    Created home.php

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<?php if(have_posts()) : ?>
+
+    <?php while (have_posts()) : the_post(); ?>
+
+      <article></article>
+
+    <?php endwhile; ?>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
