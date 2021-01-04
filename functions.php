65248c125e48e5e04107f184ecbf915e88b1dfb1
Melvin Lemoine <melvin.lemoine@feather-project.org>
2021-01-04 12:56:46 +0100

    Minor jquery import issue fixed

--- functions.php
+++ functions.php
@@ -13,8 +13,8 @@ function insert_css() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 	//########## Import all the javascript scripts here like the example below ##########
-	//wp_register_script( 'jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' );
-	//wp_enqueue_script( 'jquery' );
+	//wp_register_script( 'jquery2', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js' );
+	//wp_enqueue_script( 'jquery2' );
 }
 
 add_theme_support( 'menus' );
