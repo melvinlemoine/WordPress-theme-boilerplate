8f6b1ea3eaaaca64498b4108d764219f784f593f
Melvin Lemoine <23276163+melvinlemoine@users.noreply.github.com>
2024-04-20 12:53:31 +0200

    Added comments & enable menus feature et register nav menus

--- functions.php
+++ functions.php
@@ -1,12 +1,15 @@
 <?php
 
+// WordPress title tag management
 add_theme_support('title-tag');
 
-// ########## MENU ##########
+// ########## MENUS ##########
 
-// add_theme_support( 'menus' );
+// Enable menus feature
+add_theme_support('menus');
 
-// register_nav_menus( [
-// 	'main-menu'  => 'Main navigation',
-// 	'footer-menu'     => 'Footer navigation'
-// ] );
+// Define menus locations
+register_nav_menus([
+    'main-menu'  => 'Main navigation',
+    'footer-menu'     => 'Footer navigation'
+]);
